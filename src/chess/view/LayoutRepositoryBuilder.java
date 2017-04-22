9fd2c8458c6ebad61e88a26a0a8d5b5bd5928984
nik <nikitap4.92@gmail.com>
2017-04-22 21:37:26 +0300

    Added LayoutRepositoryBuilder

---
+++ src/chess/view/LayoutRepositoryBuilder.java
@@ -0,0 +1,76 @@
+package chess.view;
+
+import chess.domain.cell.Cell;
+import chess.domain.cell.Char;
+import chess.domain.cell.Digit;
+import chess.view.image.CellLayoutFactory;
+import chess.view.layout.CellLayoutRepository;
+import chess.view.layout.DefaultCellLayoutRepository;
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 22.04.17.
+ */
+public class LayoutRepositoryBuilder {
+    
+    private double zeroX;
+    private double zeroY;
+    private double offsetX;
+    private double offsetY;
+    private double cellWidth;
+    private double cellHeight;
+    private CellLayoutFactory factory;
+
+    public LayoutRepositoryBuilder setZeroX(double zeroX) {
+        this.zeroX = zeroX;
+        return this;
+    }
+
+    public LayoutRepositoryBuilder setZeroY(double zeroY) {
+        this.zeroY = zeroY;
+        return this;
+    }
+
+    public LayoutRepositoryBuilder setOffsetX(double offsetX) {
+        this.offsetX = offsetX;
+        return this;
+    }
+
+    public LayoutRepositoryBuilder setOffsetY(double offsetY) {
+        this.offsetY = offsetY;
+        return this;
+    }
+
+    public LayoutRepositoryBuilder setCellWidth(double cellWidth) {
+        this.cellWidth = cellWidth;
+        return this;
+    }
+
+    public LayoutRepositoryBuilder setCellHeight(double cellHeight) {
+        this.cellHeight = cellHeight;
+        return this;
+    }
+
+    public LayoutRepositoryBuilder setFactory(CellLayoutFactory factory) {
+        this.factory = factory;
+        return this;
+    }
+
+    public CellLayoutRepository build(){
+
+
+
+        DefaultCellLayoutRepository repository = new DefaultCellLayoutRepository();
+        for (Char c: Char.values())
+            for (Digit d: Digit.values()){
+                Position position = new Position(c.getOrder() * offsetX + zeroX, d.getOrder() * offsetY + zeroY);
+                Dimension dimension = new Dimension(cellHeight , cellWidth);
+
+                Cell cell = new Cell(c, d);
+                repository.put(cell, factory.create(cell, position, dimension));
+            }
+        return repository;
+    }
+
+    
+}
