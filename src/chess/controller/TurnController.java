5e35fe1f8b3f6f2f7d8125d3f9d2c474151a8ae5
nik <nikitap4.92@gmail.com>
2017-04-22 14:38:34 +0300

    Added turn controller wit

---
+++ src/chess/controller/TurnController.java
@@ -0,0 +1,15 @@
+package chess.controller;
+
+import chess.domain.piece.PieceColor;
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 22.04.17.
+ */
+public interface TurnController {
+
+    void nextTurn();
+
+    PieceColor whoseIsNext();
+
+}
---
+++ src/chess/controller/impl/DefaultTurnController.java
@@ -0,0 +1,28 @@
+package chess.controller.impl;
+
+import chess.controller.TurnController;
+import chess.domain.piece.PieceColor;
+
+import static chess.domain.piece.PieceColor.BLACK;
+import static chess.domain.piece.PieceColor.WHITE;
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 22.04.17.
+ */
+public class DefaultTurnController implements TurnController {
+
+    private PieceColor color;
+
+    @Override
+    public void nextTurn() {
+        if(color == WHITE){
+            color = BLACK;
+        }else color = WHITE;
+    }
+
+    @Override
+    public PieceColor whoseIsNext() {
+        return color;
+    }
+}
