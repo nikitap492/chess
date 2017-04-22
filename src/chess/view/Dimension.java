c76618cd1c8a95644d57fe5f6ee4be9b91585dee
nik <nikitap4.92@gmail.com>
2017-04-23 00:12:12 +0300

    Added position and dimension entity

---
+++ src/chess/view/Dimension.java
@@ -0,0 +1,25 @@
+package chess.view;
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 21.04.17.
+ */
+public class Dimension {
+
+    private double height;
+
+    private double width;
+
+    public Dimension(double height, double width) {
+        this.height = height;
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+}
---
+++ src/chess/view/Position.java
@@ -0,0 +1,24 @@
+package chess.view;
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 22.04.17.
+ */
+public class Position {
+
+    private double x;
+    private double y;
+
+    public Position(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public double getY() {
+        return y;
+    }
+}
