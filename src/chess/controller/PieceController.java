8020d292138ac02d7a0609e5306b66b708ab6833
nik <nikitap4.92@gmail.com>
2017-04-22 12:52:43 +0300

    Added controller for pieces

---
+++ src/chess/controller/PieceController.java
@@ -0,0 +1,32 @@
+package chess.controller;
+
+import chess.command.Subscriber;
+import chess.domain.cell.Cell;
+import chess.domain.piece.Piece;
+import chess.domain.piece.PieceType;
+import chess.view.PieceView;
+import chess.view.display.PieceDisplay;
+
+/**
+ * @author Poshivalov Nikita
+ * @since 20.04.2017.
+ */
+public interface PieceController extends Subscriber<PieceView> {
+
+    void setPieceDisplay(PieceDisplay pieceDisplay);
+
+    void setCellController(CellController cellController);
+
+    void setMovementController(MovementController movementController);
+
+    void setSelectController(SelectController selectController);
+
+    void move(Piece piece, Cell cell);
+
+    void kill(Piece piece);
+
+    void create(Piece piece);
+
+    void transform(Piece piece, PieceType pieceType);
+
+}
