cabc5576a5e81d6e04854615b392f0e4ffb78b64
nik <nikitap4.92@gmail.com>
2017-04-24 14:46:17 +0300

    added bishop movement analyzer

---
+++ src/chess/controller/analyzer/OrderStruct.java
@@ -0,0 +1,15 @@
+package chess.controller.analyzer;
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 24.04.17.
+ */
+class OrderStruct {
+    OrderStruct(int charOrder, int digitOrder) {
+        this.charOrder = charOrder;
+        this.digitOrder = digitOrder;
+    }
+
+    int charOrder;
+    int digitOrder;
+}
