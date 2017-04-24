180e8bd1a56d070390fecdf92b4ff0616f48e7b8
nik <nikitap4.92@gmail.com>
2017-04-24 21:15:53 +0300

    Added knight movement analyzer

---
+++ src/chess/controller/analyzer/KnightMovementAnalyzer.java
@@ -0,0 +1,31 @@
+package chess.controller.analyzer;
+
+import chess.controller.CheckmateController;
+import chess.controller.PieceController;
+import chess.domain.movement.Movement;
+import chess.domain.piece.Piece;
+
+import java.util.Set;
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 24.04.17.
+ */
+class KnightMovementAnalyzer extends PieceMovementAnalyzer {
+
+    KnightMovementAnalyzer(PieceController pieceController, CheckmateController checkmateController) {
+        super(pieceController, checkmateController);
+    }
+
+    @Override
+    void addMovements(Set<Movement> empty, Piece piece) {
+        addMovement(empty, piece, Directions::northEastEast);
+        addMovement(empty, piece, Directions::northNorthEast);
+        addMovement(empty, piece, Directions::northNorthWest);
+        addMovement(empty, piece, Directions::northWestWest);
+        addMovement(empty, piece, Directions::southEastEast);
+        addMovement(empty, piece, Directions::southWestWest);
+        addMovement(empty, piece, Directions::southSouthEast);
+        addMovement(empty, piece, Directions::southSouthWest);
+    }
+}
