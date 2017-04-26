f527647d6cf1aceaa88f71c1aa65a9d82e3dbe0e
nikita <nikitap4.92@gmail.com>
2017-04-26 21:06:29 +0300

    copy constructor

--- src/chess/domain/piece/Piece.java
+++ src/chess/domain/piece/Piece.java
@@ -11,12 +11,21 @@ public class Piece {
     private PieceType type;
     private PieceColor color;
     private Cell cell;
-    private boolean alive;
+    private boolean alive = true;
+    private boolean moved = false;
 
     public Piece(PieceType type, PieceColor color) {
         this.type = type;
         this.color = color;
         this.alive = true;
+    }
+
+    public Piece(Piece piece){
+        this.cell = piece.cell;
+        this.color = piece.color;
+        this.type = piece.type;
+        this.alive = piece.alive;
+        this.moved = piece.moved;
     }
 
     public PieceType getType() {
@@ -47,4 +56,33 @@ public class Piece {
         this.type = type;
     }
 
+    public boolean isMoved() {
+        return moved;
+    }
+
+    public void setMoved(boolean moved) {
+        this.moved = moved;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Piece piece = (Piece) o;
+
+        if (type != piece.type) return false;
+        if (color != piece.color) return false;
+        return cell != null ? cell.equals(piece.cell) : piece.cell == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = type.hashCode();
+        result = 31 * result + color.hashCode();
+        result = 31 * result + (cell != null ? cell.hashCode() : 0);
+        return result;
+    }
 }
