7bfe07384fc4c461c5f23a349d6ebf8dbabfdcfc
nik <nikitap4.92@gmail.com>
2017-04-26 11:27:14 +0300

    model fixes

--- src/chess/domain/cell/Char.java
+++ src/chess/domain/cell/Char.java
@@ -1,11 +1,19 @@
 package chess.domain.cell;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Poshivalov Nikita
  * @since 14.04.2017.
  */
 public enum Char {
     A(0), B(1), C(2), D(3), E(4), F(5), G(6), H(7);
+    private static final List<Char> chars = new ArrayList<>();
+    static {
+        chars.addAll(Arrays.asList(Char.values()));
+    }
 
     Char(int order) {
         this.order = order;
@@ -15,6 +23,10 @@ public enum Char {
 
     public int getOrder() {
         return order;
+    }
+
+    public static Char get(int order){
+        return chars.get(order);
     }
 }
 
--- src/chess/domain/cell/Digit.java
+++ src/chess/domain/cell/Digit.java
@@ -1,25 +1,30 @@
 package chess.domain.cell;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author Poshivalov Nikita
  * @since 14.04.2017.
  */
 public enum Digit {
     ONE(0), TWO(1), THREE(2), FOUR(3), FIVE(4), SIX(5), SEVEN(6), EIGHT(7);
-    private static final int step = 82;
-    private static final int offset = 40;
+    private static final List<Digit> digits = new ArrayList<>();
+    static {
+        digits.addAll(Arrays.asList(Digit.values()));
+    }
 
     Digit(int order) {
         this.order = order;
-        this.y = (7 - order) * step + offset;
     }
 
-    private final int y;
     private final int order;
 
-    public int getY() {
-        return y;
-    }
     public int getOrder() { return order;}
+
+    public static Digit get(int order){
+        return digits.get(order);
+    }
 }
 
