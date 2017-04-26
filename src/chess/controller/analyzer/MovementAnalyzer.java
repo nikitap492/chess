9ab73e2fe69fc03f7f5b414e725d04e9e648ecab
nik <nikitap4.92@gmail.com>
2017-04-26 13:02:28 +0300

    Added movementAnalyzer

---
+++ src/chess/controller/analyzer/MovementAnalyzer.java
@@ -0,0 +1,97 @@
+package chess.controller.analyzer;
+
+import chess.controller.CheckmateController;
+import chess.controller.MovementController;
+import chess.controller.PieceController;
+import chess.controller.TurnController;
+import chess.domain.cell.Cell;
+import chess.domain.movement.Movement;
+import chess.domain.piece.Piece;
+import chess.domain.piece.PieceType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
+import static chess.domain.movement.MovementType.CHECK;
+
+
+/**
+ * Created by nikitap4.92@gmail.com
+ * 22.04.17.
+ */
+public class MovementAnalyzer implements MovementController {
+
+    private List<Movement> movements;
+    private Set<Movement> possibleMovements;
+    private Piece piece;
+    private final BishopMovementAnalyzer bishopMovementAnalyzer;
+    private final RookMovementAnalyzer rookMovementAnalyzer;
+    private final QueenMovementAnalyzer queenMovementAnalyzer;
+    private final KingMovementAnalyzer kingMovementAnalyzer;
+    private final PawnMovementAnalyser pawnMovementAnalyzer;
+    private final KnightMovementAnalyzer knightMovementAnalyzer;
+    private final PieceController pieceController;
+    private final TurnController turnController;
+    private final CheckmateController checkmateController;
+
+    public MovementAnalyzer(PieceController pieceController, CheckmateController checkmateController, TurnController turnController) {
+        bishopMovementAnalyzer = new BishopMovementAnalyzer(pieceController, checkmateController);
+        rookMovementAnalyzer = new RookMovementAnalyzer(pieceController, checkmateController);
+        queenMovementAnalyzer = new QueenMovementAnalyzer(pieceController, checkmateController);
+        kingMovementAnalyzer = new KingMovementAnalyzer(pieceController, checkmateController);
+        pawnMovementAnalyzer = new PawnMovementAnalyser(pieceController, checkmateController);
+        knightMovementAnalyzer = new KnightMovementAnalyzer(pieceController, checkmateController);
+        this.pieceController = pieceController;
+        this.turnController = turnController;
+        this.checkmateController = checkmateController;
+        movements = new ArrayList<>();
+    }
+
+    @Override
+    public Set<Movement> possibleMovements(Piece piece) {
+        possibleMovements = null;
+        this.piece = piece;
+        PieceType type = piece.getType();
+        switch (type){
+            case PAWN:
+                possibleMovements = pawnMovementAnalyzer.analyze(piece);
+                break;
+            case KNIGHT:
+                possibleMovements = knightMovementAnalyzer.analyze(piece);
+                break;
+            case BISHOP:
+                possibleMovements = bishopMovementAnalyzer.analyze(piece);
+                break;
+            case ROOK:
+                possibleMovements = rookMovementAnalyzer.analyze(piece);
+                break;
+            case QUEEN:
+                possibleMovements = queenMovementAnalyzer.analyze(piece);
+                break;
+            case KING:
+                possibleMovements = kingMovementAnalyzer.analyze(piece);
+                break;
+        }
+        return possibleMovements;
+    }
+
+    @Override
+    public void moveToCell(Cell cell) {
+        Optional<Movement> any = possibleMovements.stream()
+                .filter(movement -> movement.getTo().equals(cell))
+                .findFirst();
+        any.ifPresent(this::doMove);
+    }
+
+    private void doMove(Movement movement) {
+        movements.add(movement);
+        pieceController.move(piece, movement.getTo());
+        turnController.nextTurn();
+        if(movement.getType() == CHECK){
+            checkmateController.check();
+        }
+    }
+
+}
