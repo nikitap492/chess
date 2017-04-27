333a2f00f285ef427427ba12439b96efe2cb8f3e
nik <nikitap4.92@gmail.com>
2017-04-27 22:58:25 +0300

    CellController refactoring

--- src/chess/controller/CellController.java
+++ src/chess/controller/CellController.java
@@ -2,8 +2,8 @@ package chess.controller;
 
 import chess.command.Subscriber;
 import chess.domain.cell.Cell;
+import chess.domain.cell.CellSelection;
 import chess.view.CellView;
-import chess.view.display.CellDisplay;
 
 /**
  * Created by nikitap4.92@gmail.com
@@ -11,17 +11,9 @@ import chess.view.display.CellDisplay;
  */
 public interface CellController extends Subscriber<CellView>{
 
-    void setCellDisplay(CellDisplay cellDisplay);
-
     void setMovementController(MovementController movementController);
 
-    void select(Cell cell);
-
-    void treat(Cell cell);
-
-    void free(Cell cell);
-
-    void check(Cell cell);
-
     void clear();
+
+    void display(Cell cell, CellSelection type);
 }
--- src/chess/controller/impl/DefaultCellController.java
+++ src/chess/controller/impl/DefaultCellController.java
@@ -4,10 +4,9 @@ import chess.command.Click;
 import chess.controller.CellController;
 import chess.controller.MovementController;
 import chess.domain.cell.Cell;
+import chess.domain.cell.CellSelection;
 import chess.view.CellView;
 import chess.view.display.CellDisplay;
-
-import static chess.domain.cell.CellSelection.*;
 
 /**
  * Created by nikitap4.92@gmail.com
@@ -21,10 +20,6 @@ public class DefaultCellController implements CellController {
         this.cellDisplay = cellDisplay;
     }
 
-    @Override
-    public void setCellDisplay(CellDisplay cellDisplay) {
-        this.cellDisplay = cellDisplay;
-    }
 
     @Override
     public void setMovementController(MovementController movementController) {
@@ -32,32 +27,14 @@ public class DefaultCellController implements CellController {
     }
 
     @Override
-    public void select(Cell cell) {
-        cell.setSelection(SELECT);
-        cellDisplay.put(cell);
-    }
-
-    @Override
-    public void treat(Cell cell) {
-        cell.setSelection(TREAT);
-        cellDisplay.put(cell);
-    }
-
-    @Override
-    public void free(Cell cell) {
-        cell.setSelection(FREE);
-        cellDisplay.put(cell);
-    }
-
-    @Override
-    public void check(Cell cell) {
-        cell.setSelection(CHECK);
-        cellDisplay.put(cell);
-    }
-
-    @Override
     public void clear() {
         cellDisplay.clear();
+    }
+
+    @Override
+    public void display(Cell cell, CellSelection type) {
+        cell.setSelection(type);
+        cellDisplay.put(cell);
     }
 
 
@@ -65,6 +42,5 @@ public class DefaultCellController implements CellController {
     public void update(Click<CellView> click) {
         Cell cell = click.target().cell();
         movementController.moveToCell(cell);
-        clear();
     }
 }
