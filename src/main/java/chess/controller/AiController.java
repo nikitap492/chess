6a70586a1887b3654ae93d08b34e0ef19032dc44
nikitap <nikitap4.92@gmail.com>
2017-07-20 08:56:51 +0300

    Added AI

---
+++ src/main/java/chess/controller/AiController.java
@@ -0,0 +1,11 @@
+package chess.controller;
+
+/**
+ * @author Poshivalov N.A.
+ * @since 18.07.2017.
+ */
+public interface AiController {
+
+     void doMove();
+
+}
---
+++ src/main/java/chess/controller/analyzer/MovementValueAnalyzer.java
@@ -0,0 +1,56 @@
+package chess.controller.analyzer;
+
+import chess.controller.MovementController;
+import chess.domain.movement.Movement;
+import chess.domain.piece.PieceType;
+
+import java.util.Comparator;
+
+/**
+ * @author Poshivalov N.A.
+ * @since 18.07.2017.
+ */
+public class MovementValueAnalyzer {
+    private final MovementController analyzer;
+
+    public MovementValueAnalyzer(MovementController analyzer) {
+        this.analyzer = analyzer;
+    }
+
+    private static int relativeValue(PieceType pieceType){
+        switch (pieceType){
+            case PAWN:
+                return 1;
+            case KNIGHT:
+                return 3;
+            case BISHOP:
+                return 3;
+            case ROOK:
+                return 5;
+            case QUEEN:
+                return 9;
+            case KING:
+                return 999;
+        }
+        return 0;
+    }
+
+    public Movement lowValue(){
+        return analyzer.possible()
+                .max(new MovementComparator())
+                .orElseThrow(RuntimeException::new);
+    }
+
+
+    private static class MovementComparator implements Comparator<Movement>{
+
+        @Override
+        public int compare(Movement o1, Movement o2) {
+            int first = o1.getKilled().isPresent() ? relativeValue(o1.getKilled().get().getType()) : 0;
+            int second = o2.getKilled().isPresent() ? relativeValue(o2.getKilled().get().getType()) : 0;
+
+            return first - second;
+        }
+    }
+    
+}
